bb272db93dbc7c56e197954949369a9465e3501b
emiliano <badiyaxo03@gmail.com>
2021-09-14 01:33:55 -0500

    fix port

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
+const PORT = process.env.PORT || 3000;
+
+
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
@@ -11,6 +14,8 @@ router.get('/',function(req,res){
 //add the router
 app.use('/', router);
 app.use(express.static('public'));
-app.listen(process.env.port || 3000);
+app.listen(PORT, () => {
+  console.log(`Our app is running on port ${ PORT }`);
+});
 
 console.log('Running at Port 3000');
